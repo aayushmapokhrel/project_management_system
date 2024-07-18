79c3c240ded6685a5d494866777651c217efb2e2
unknown <jitendrakarki988036@gmail.com>
2024-07-18 17:42:00 +0545

    Add crud to sprint

--- task/api/urls.py
+++ task/api/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from task.api.views import(
             TaskAPIView,
             TaskUpdateView,
+            SprintAPIView,
+            SprintUpdateView
 )
 
 urlpatterns = [
     path('', TaskAPIView.as_view(), name='task_view'),
     path('<int:pk>', TaskUpdateView.as_view(), name='task_update'),
+    path('sprint/', SprintAPIView.as_view(), name='sprint_view'),
+    path('sprint/<int:pk>', SprintUpdateView.as_view(), name='task_update'),
 
 
 ]
