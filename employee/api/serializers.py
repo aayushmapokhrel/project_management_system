53a90359cbdbb6f94d961404b4e2c7841eabc83c
Sudan Bhandari <sudan1bhandari@gmail.com>
2024-07-18 18:22:12 +0545

    Merge confilict

--- employee/api/serializers.py
+++ employee/api/serializers.py
@@ -7,12 +7,28 @@ class EmployeeSerializer(serializers.ModelSerializer):
     designation = serializers.PrimaryKeyRelatedField(
         queryset=Designation.objects.all()
         )
+    email = serializers.EmailField()
+    phone = serializers.IntegerField()
     name = serializers.CharField(required=True)
     address = serializers.CharField(required=True)
 
     class Meta:
         model = Employee
         exclude = ["created_by", "modified_by"]
+    
+    def validate_email(self, value):
+        if Employee.objects.filter(email=value):
+            raise serializers.ValidationError(
+                "An employee with this email already exists."
+            )
+        return value
+
+    def validate_phone(self, value):
+        if Employee.objects.filter(phone=value):
+            raise serializers.ValidationError(
+                "An employee with this phone number already exists."
+            )
+        return value
 
 
 class ReadEmployeeSerializer(serializers.ModelSerializer):
@@ -27,3 +43,5 @@ class ReadEmployeeSerializer(serializers.ModelSerializer):
         data['designation_name'] = designation.name
         data['department_name'] = designation.department.name
         return data
+    
+    
